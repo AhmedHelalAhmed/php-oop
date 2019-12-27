0e4456102d1fc158652003db9fb3478bc3b69e21
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-12-27 19:25:48 +0200

    Add example #4

---
+++ example_4.php
@@ -0,0 +1,34 @@
+class AppleDevice {
+
+// properties
+
+public $ram;
+public $inch;
+public $space;
+
+}
+
+$iphone6plus = new AppleDevice();
+$iphone6plus->ram='2GB';
+$iphone6plus->inch='5 inch';
+$iphone6plus->space='32GB';
+$iphone6plus->color='gold';// !! this property does not exists
+echo '<pre>';
+var_dump($iphone6plus);
+echo '</pre>';
+
+
+$iphone7plus = new AppleDevice();
+$iphone7plus->ram='4GB';
+$iphone7plus->inch='5.5 inch';
+$iphone7plus->space='256GB';
+$iphone7plus->colors='silver';// !! this property does not exists
+echo '<pre>';
+var_dump($iphone7plus);
+echo '</pre>';
+
+
+$iphone = new AppleDevice();
+echo '<pre>';
+var_dump($iphone );
+echo '</pre>';
