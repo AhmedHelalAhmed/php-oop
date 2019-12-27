aa8ea84967d91b541cd9ae13d0591d4e816db083
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-12-27 19:13:10 +0200

    Add example #1

---
+++ example_1.php
@@ -0,0 +1,17 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class AppleDevice
+{
+
+}
+
+$iphone6plus = new AppleDevice();
+
+echo '<pre>';
+
+var_dump($iphone6plus);
+
+echo '</pre>';
