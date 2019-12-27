4f9af6ad9bd34710806773cb14315ac23fc393fa
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-12-27 19:33:12 +0200

    Add example #6 and fix #4 and #4

--- example_4.php
+++ example_4.php
@@ -1,34 +1,38 @@
-class AppleDevice {
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class AppleDevice
+{
 
 // properties
 
-public $ram;
-public $inch;
-public $space;
+    public $ram;
+    public $inch;
+    public $space;
 
 }
 
 $iphone6plus = new AppleDevice();
-$iphone6plus->ram='2GB';
-$iphone6plus->inch='5 inch';
-$iphone6plus->space='32GB';
-$iphone6plus->color='gold';// !! this property does not exists
+$iphone6plus->ram = '2GB';
+$iphone6plus->inch = '5 inch';
+$iphone6plus->space = '32GB';
+$iphone6plus->color = 'gold'; // !! this property does not exists
 echo '<pre>';
 var_dump($iphone6plus);
 echo '</pre>';
 
-
 $iphone7plus = new AppleDevice();
-$iphone7plus->ram='4GB';
-$iphone7plus->inch='5.5 inch';
-$iphone7plus->space='256GB';
-$iphone7plus->colors='silver';// !! this property does not exists
+$iphone7plus->ram = '4GB';
+$iphone7plus->inch = '5.5 inch';
+$iphone7plus->space = '256GB';
+$iphone7plus->colors = 'silver'; // !! this property does not exists
 echo '<pre>';
 var_dump($iphone7plus);
 echo '</pre>';
 
-
 $iphone = new AppleDevice();
 echo '<pre>';
-var_dump($iphone );
+var_dump($iphone);
 echo '</pre>';
--- example_6.php
+++ example_6.php
@@ -6,12 +6,20 @@ error_reporting(E_ALL);
 class AppleDevice
 {
 
-// properties
+    // properties
 
-    public $ram;
+    public $ram = '1GB';
     public $inch;
     public $space;
     public $color;
+
+    // methods
+
+    public function doubleHomePressed()
+    {
+        // echo 'you have pressed the home button twice';
+        echo 'this device does not support this feature';
+    }
 }
 
 $iphone6plus = new AppleDevice();
@@ -19,11 +27,13 @@ $iphone6plus->ram = '2GB';
 $iphone6plus->inch = '5 inch';
 $iphone6plus->space = '32GB';
 $iphone6plus->color = 'gold';
+$iphone6plus->doubleHomePressed();
 echo '<pre>';
 var_dump($iphone6plus);
 echo '</pre>';
 
 $iphone7plus = new AppleDevice();
+
 $iphone7plus->ram = '4GB';
 $iphone7plus->inch = '5.5 inch';
 $iphone7plus->space = '256GB';
@@ -33,6 +43,7 @@ var_dump($iphone7plus);
 echo '</pre>';
 
 $iphone = new AppleDevice();
+
 echo '<pre>';
 var_dump($iphone);
 echo '</pre>';
