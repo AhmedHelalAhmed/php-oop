48b2b1896f65e1061d1cf61b79c26d6d8c537538
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-12-27 19:19:00 +0200

    Add example #3

---
+++ example_3.php
@@ -0,0 +1,28 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+class AppleDevice
+{
+
+    // properties
+
+    public $ram = '2GB';
+    public $inch;
+    public $space;
+    public $color;
+
+    // note:  var still exists and = public
+
+}
+
+$iphone6plus = new AppleDevice();
+echo '<pre>';
+var_dump($iphone6plus);
+echo '</pre>';
+
+$iphone7plus = new AppleDevice();
+echo '<pre>';
+var_dump($iphone7plus);
+echo '</pre>';
