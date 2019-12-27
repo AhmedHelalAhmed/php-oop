1715dd80e9771bdfc3dd486aa0b1e7474f524d39
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-12-27 19:09:18 +0200

    Add basics

---
+++ basics.php
@@ -0,0 +1,48 @@
+<?php
+/*
+
+Class & Object
+
+- Class is a blueprint that u can create object from
+- object is a member in the main application
+- class has properties
+- variable inside class = property
+- variable outside class = variable
+- function inside class = method
+- function outside class = function
+- [class] = class keyword
+- [new] = new object
+- [public, private, protected]
+- [->] = object operator
+- [ $this ] = psudo variable [ refer to object properties ]
+- [ :: ] = scope resolution operator [ pammayim nekudotayim ] = double colon
+
+Apple
+- Class       = Apple blueprint Design
+- Object      = iphone that China made
+- Application = Apple store
+
+Web Application Registeration
+- class       = code to add new member
+- object      = the member
+- Application = web application registeration
+
+Blog system
+- class       = code to add new post,  article, news, information
+- object      = post, article, news, information
+- Apllication = blog system
+
+$this vs self
+
+-self:
+
+- refer to current class
+- access static members
+- not use ( $ ) because its not represent variable but present class construction
+
+-$this:
+- refer to current object
+- access non static varible
+- use ($) vecause it represent a variable
+
+ */
