b7e58606c7c5f2c9bf079a0b083c46c2885cb038
Ahmed Helal Ahmed <ahmed.helal.online@gmail.com>
2019-12-27 21:10:48 +0200

    Add example #19 - not give me the error of standard code

---
+++ example_19.php
@@ -0,0 +1,63 @@
+<?php
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+ini_set('error_reporting', E_ALL);
+
+class AppleDevice
+{
+
+    // properties
+
+    public $ram = '1GB';
+    public $inch = '4 Inch';
+    public $space = '16 GB';
+    public $color = 'Silver';
+    public $screen = 'LCD';
+
+    // methods
+
+    public function changeSpec($r, $in, $ps, $co)
+    {
+        $this->ram = $r;
+        $this->inch = $in;
+        $this->space = $ps;
+        $this->color = $co;
+
+    }
+
+}
+
+class Sony extends AppleDevice
+{
+
+    // properties
+
+    public $camera = '25MB';
+
+    // methods
+    public function changeSpec($r, $in, $ps, $co, $ca)
+    {
+        // ? this will give error but not giving error
+        $this->ram = $r;
+        $this->inch = $in;
+        $this->space = $ps;
+        $this->color = $co;
+        $this->camera = $ca;
+
+    }
+
+}
+
+$iphone6plus = new AppleDevice();
+$iphone6plus->changeSpec('2GB', '5 inch', '32GB', 'Gold');
+
+echo '<pre>';
+print_r($iphone6plus);
+echo '</pre>';
+
+$sony = new Sony();
+$sony->changeSpec('2GB', '5 inch', '32GB', 'Gold', '30MB');
+
+echo '<pre>';
+print_r($sony);
+echo '</pre>';
